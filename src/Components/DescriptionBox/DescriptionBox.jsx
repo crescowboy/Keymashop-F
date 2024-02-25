33797c90f64bea2e0517947246296b17654bab58
wcrespo <wellingtoncrespo2001@gmail.com>
2024-02-24 21:04:32 -0500

    Create DescriptionBox component

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="description-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores minima nostrum eum dolore blanditiis quod nam aut cum praesentium modi temporibus molestias illo, repellat quaerat nisi aspernatur recusandae perspiciatis consectetur.
+            Voluptas nobis qui beatae cum, eum quam ex aliquam omnis aspernatur, odit dolorum optio soluta dolore quas ducimus ab voluptate alias porro quos laudantium obcaecati temporibus totam sequi? Pariatur, accusantium.
+            Error libero quam vero impedit, voluptatum nostrum quibusdam velit, animi debitis, dolorum explicabo obcaecati et? Illum fugiat tenetur harum vitae asperiores ad, quisquam atque, laboriosam dignissimos, quidem nobis! Similique, perspiciatis!
+            Nisi, quia quos expedita eveniet saepe sunt delectus ut, inventore sapiente laborum nam quibusdam ab fuga vero blanditiis! Itaque dolor atque similique dolores magnam quam labore tempore nemo distinctio eaque.
+            Porro quo libero, ratione at adipisci quae, commodi saepe itaque odio reprehenderit temporibus natus, necessitatibus deleniti consequuntur! Nisi, eos quaerat nostrum alias ea delectus ipsam quibusdam sed. Explicabo, repudiandae nisi?
+            </p>
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, laborum repellendus ducimus sed enim inventore architecto quod assumenda, voluptatum veniam placeat ea, aperiam maiores dolorum sequi. Qui quasi fugit inventore.
+            Itaque amet quas corrupti numquam reprehenderit inventore blanditiis ad, vel impedit voluptatum sit fugit pariatur quasi saepe magni excepturi et repellendus nihil provident enim fuga sequi in tenetur suscipit? Ratione.
+            Error culpa quod exercitationem optio similique qui quam at modi natus esse. Velit corrupti nulla ducimus ut natus culpa eaque maiores, cumque ipsam, mollitia sapiente provident earum hic, illo veritatis!  
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
