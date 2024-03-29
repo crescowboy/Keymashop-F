4860c344a24ca11f115f4484a958ed01a4e270f5
wcrespo <wellingtoncrespo2001@gmail.com>
2024-03-28 22:33:17 -0500

    update footer0

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import footer_logo from '../Assets/logo_big.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
@@ -12,13 +14,13 @@ const Footer = () => {
             <img src={footer_logo} alt="" />
             <p>SHOOPER</p>
         </div>
-        <ul className='footer-links'>
-            <li>Company</li>
-            <li>Products</li>
-            <li>Offices</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
+        <div className='footer-links'>
+            <Link to={"/"}>Shop</Link>
+            <Link to={"/womens"}>Womens</Link>
+            <Link to={"/mens"}>Mens</Link>
+            <Link to={"/kids"}>Kids</Link>
+            <Link to={"/"}>Contact</Link>
+        </div>
         <div className="footer-social-icon">
             <div className="footer-icons-container">
                 <img src={instagram_icon} alt="" />
