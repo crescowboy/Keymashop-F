5d3503e015dd6d04507ab04bd98faab21880446d
wcrespo <wellingtoncrespo2001@gmail.com>
2024-04-12 22:44:16 -0500

    agregando canmtidad maxima que se puede mostrar por categoria

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,38 +1,46 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
-
+import React, { useContext, useState } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
+  const [visibleProducts, setVisibleProducts] = useState(16); // Número de productos visibles inicialmente
+  const productsToShow = all_product.filter(item => props.category === item.category).slice(0, visibleProducts);
+  const totalProductsInCategory = all_product.filter(item => props.category === item.category).length;
+
+  const loadMore = () => {
+    const remainingProducts = all_product.filter(item => props.category === item.category).length - visibleProducts;
+    const productsToAdd = Math.min(12, remainingProducts); // Calcula la cantidad de productos que se pueden agregar
+  
+    setVisibleProducts(prevVisibleProducts => prevVisibleProducts + productsToAdd); // Añade la cantidad calculada de productos
+  };
+  
+
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+        <span>Showing 1-{visibleProducts}</span> out of {totalProductsInCategory} products
         </p>
         <div className='shopcategory-sort'>
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item, i)=>{
-          if (props.category===item.category){
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          }
-          else{
-            return null;
-          }
-        })}
+        {productsToShow.map((item, i) => (
+          <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        ))}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>
+      {visibleProducts < all_product.filter(item => props.category === item.category).length && (
+        <div className="shopcategory-loadmore" onClick={loadMore}>
+          Explore More
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
