0ef62c33dd9ffcc579f1037d9d70adf621e7dca7
wcrespo <wellingtoncrespo2001@gmail.com>
2024-04-27 22:17:15 -0500

    mercado pago frontend

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -8,8 +8,45 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import axios from "axios";
+
+
+
+
+
+
 
 const ProductDisplay = (props) => {
+  const [preferenceId, setPreferenceId] = useState(null)
+
+  initMercadoPago('YOUR_PUBLIC_KEY', {
+    locale: "es-AR",
+  });
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/create_preference",{
+        title: "Bananita contenta",
+        quantity: 1,
+        price: 100,
+      });
+
+      const { id } = response.data;
+      return id;
+    }catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    if (id) {
+      setPreferenceId(id);
+    }
+  }
+
+
   const {product} = props;
   const [mainImage, setMainImage] = useState(null);
 
@@ -83,6 +120,10 @@ const ProductDisplay = (props) => {
         </div>
         <div className='container-cart-wsp'>
         <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+        <button onClick={handleBuy}>comprar</button>
+        {preferenceId && <Wallet initialization={{ preferenceId: preferenceId }} customization={{ texts:{ valueProp: 'smart_option'}}} />}
+        
+        
         <a className='btn-wsp' href={`https://wa.me/593994084941?text=${encodeURIComponent(mensaje)}`} target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faWhatsapp} />
         Comprar por WhatsApp
