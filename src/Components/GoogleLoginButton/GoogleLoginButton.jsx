6b64e25811ca9c83286878bf432330bd53e9e8ce
wcrespo <wellingtoncrespo2001@gmail.com>
2024-05-20 14:30:34 -0500

    agregando permanencia de google login

--- src/Components/GoogleLoginButton/GoogleLoginButton.jsx
+++ src/Components/GoogleLoginButton/GoogleLoginButton.jsx
@@ -1,51 +1,79 @@
-import { useEffect, useState } from "react";
-import { GoogleLogin } from 'react-google-login';
+import React, { useEffect, useState } from "react";
+import { GoogleLogin } from "react-google-login";
 import { gapi } from "gapi-script";
 
-import React from 'react'
-
 const GoogleLoginButton = () => {
+  const client_ID = "459220392607-5tqfp24sjpko70hlhppmgag1mjcse84a.apps.googleusercontent.com";
+  const [user, setUser] = useState(null);
 
-    const client_ID = "459220392607-5tqfp24sjpko70hlhppmgag1mjcse84a.apps.googleusercontent.com";
-    const [user, setUser] = useState({});
+  useEffect(() => {
+    const initClient = () => {
+      gapi.auth2.init({
+        clientId: client_ID,
+        scope: "profile email"
+      });
+    };
 
+    gapi.load("client:auth2", initClient);
 
-    useEffect(()=>{
-        const start = () =>{
-            gapi.auth2.init({
-                clientId: client_ID,
-            })
-        }
-        gapi.load("client:auth2", start)
-    }, [])
-
-
-    const onSuccess = (response) =>{
-        setUser(response.profileObj);
+    // Check if user data is available in localStorage on component mount
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setUser(storedUser);
     }
+  }, [client_ID]);
 
-    const onFailure = () => {
-        console.log("Something went wrong")
+  const onSuccess = async (response) => {
+    const profile = response.profileObj;
+    localStorage.setItem("user", JSON.stringify(profile));
+    localStorage.setItem("accessToken", response.accessToken);
+    setUser(profile); // Update the user state
+
+    // Enviar el token al backend
+    const idToken = response.tokenId;
+    const backendResponse = await fetch('http://localhost:4000/google-login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token: idToken }),
+    }).then(res => res.json());
+
+    if (backendResponse.success) {
+      localStorage.setItem('auth-token', backendResponse.token);
+      window.location.replace("/");
     }
+  };
+
+  const onFailure = (error) => {
+    console.error("Login Failure:", error);
+  };
 
   return (
     <div>
-    <div>
-  
-    <GoogleLogin
+      <GoogleLogin
         clientId={client_ID}
         onSuccess={onSuccess}
         onFailure={onFailure}
-        cookiePolicy={"single_host_policy"}
-    />
+        cookiePolicy={"single_host_origin"}
+        render={(renderProps) => (
+          <button
+            className="google-login-button"
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            Login with Google
+          </button>
+        )}
+      />
+      {user && (
+        <div className="profile">
+          <img src={user.imageUrl} alt={user.name} />
+          <h3>{user.name}</h3>
+        </div>
+      )}
     </div>
-    <div className={user? "profile":"hidden"}>
-        <img src={user.imageUrl} alt=""/>
-        <h3>{user.name}</h3>
-    </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default GoogleLoginButton
+export default GoogleLoginButton;
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,85 +1,87 @@
-import React, { useState } from 'react'
-import './CSS/LoginSignup.css'
-import GoogleLogin from 'react-google-login'
-import GoogleLoginButton from '../Components/GoogleLoginButton/GoogleLoginButton'
+import React, { useState, useEffect } from 'react';
+import './CSS/LoginSignup.css';
+import GoogleLoginButton from '../Components/GoogleLoginButton/GoogleLoginButton';
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: ""
+  });
 
-  const [state,setState] = useState("Login")
-  const [formData,setFormData] = useState({
-    username:"",
-    password:"",
-    email:""
-  })
-
-  const changeHandler = (e) =>{
-    setFormData({...formData,[e.target.name]:e.target.value})
-  }
-
-  const login = async () =>{
-    console.log("Login Function Executed",formData)
-    let responseData;
-    await fetch('http://localhost:4000/login',{
-      method:'POST',
-      headers:{
-        Accept:'application/form-data',
-        'Content-Type':'application/json',
-      },
-      body: JSON.stringify(formData),
-    }).then((response)=> response.json()).then((data)=>responseData=data)
-  
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+  useEffect(() => {
+    const token = localStorage.getItem('auth-token');
+    if (token) {
       window.location.replace("/");
     }
-    else{
-      alert(responseData.errors)
-    }
-  }
+  }, []);
 
-  const signUp = async () =>{
-    console.log("SignUp Function Executed",formData)
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const login = async () => {
+    console.log("Login Function Executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
-      method:'POST',
-      headers:{
-        Accept:'application/form-data',
-        'Content-Type':'application/json',
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
-    }).then((response)=> response.json()).then((data)=>responseData=data)
-  
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token);
+    }).then((response) => response.json()).then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
       window.location.replace("/");
+    } else {
+      alert(responseData.errors);
     }
-    else{
-      alert(responseData.errors)
+  };
+
+  const signUp = async () => {
+    console.log("SignUp Function Executed", formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => response.json()).then((data) => responseData = data);
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
     }
-  }
+  };
 
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name'/>:<></>}
-          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address'/>
+          {state === "Sign Up" ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' /> : <></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address' />
           <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
         </div>
-        <button onClick={()=>{state==="Login"?login():signUp()}}>Continue</button>
-        {state==="Sign Up"
-        ?<p className='loginsignup-login'>Already have an account <span onClick={()=>{setState("Login")}}>Login here</span></p>
-        :<p className='loginsignup-login'>Create an account <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
+        <button onClick={() => { state === "Login" ? login() : signUp() }}>Continue</button>
+        {state === "Sign Up"
+          ? <p className='loginsignup-login'>Already have an account <span onClick={() => { setState("Login") }}>Login here</span></p>
+          : <p className='loginsignup-login'>Create an account <span onClick={() => { setState("Sign Up") }}>Click here</span></p>}
         <div className="loginsignup-agree">
-          <input type="checkbox" name='' id=''/>
-          <p>By continuing, i agree to the terms of use & privacy policy.</p>
-          
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
-        <GoogleLoginButton></GoogleLoginButton>
+        <GoogleLoginButton />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
