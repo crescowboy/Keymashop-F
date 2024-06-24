ebb679302ea8322d68742f8b7910fd4da5620afe
wcrespo <wellingtoncrespo2001@gmail.com>
2024-06-24 13:46:47 -0500

    validacion productos categoria

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -3,6 +3,8 @@ import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
 import dropdown_icon from '../Components/Assets/dropdown_icon.png';
 import Item from '../Components/Item/Item';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
@@ -38,9 +40,13 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div id="shopcategory-products" className="shopcategory-products">
-        {productsToShow.map((item, i) => (
-          <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-        ))}
+        {all_product ? (
+          productsToShow.map((item, i) => (
+            <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          ))
+          ):( <div className="loading-spinner">No hay productos disponibles...</div>
+        )}
+        
       </div>
       {visibleProducts < all_product.filter(item => props.category === item.category).length && (
         <div className="shopcategory-loadmore" onClick={loadMore}>
